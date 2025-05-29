07dfe4ac3748aa1677fd25d6c32aa8fe351f8bb9
SQLMDX Clients <195984129+SQLMDXClients@users.noreply.github.com>
2025-05-29 18:05:45 -0400

    Add files via upload

---
+++ step 4 compare schemas.sql
@@ -0,0 +1,29 @@
+-- Schema Monitor has several procedures to compare schemas.  Please see the
+-- documentation for more information on the differences between these procedures.
+
+-- The assumption will have been made that step 3 push multiple databases has been 
+-- completed several times over a period of time.   We will compare database to
+-- themselves to see what has changed over that time.   We will also compare 
+-- different databases in a family to make sure that they are in sync.
+
+-- This will be our base procedure for comparing schemas.
+
+-- _CODE.SCHEMA_COMPARE( BASE_DATABASE_NAME STRING, BASE_SCHEMA_DATE TIMESTAMP_TZ,
+--                      TARGET_DATABASE_NAME STRING, TARGET_SCHEMA_DATE TIMESTAMP_TZ,
+--                      DEBUG_TO_LOG BOOLEAN DEFAULT FALSE)
+
+--    FAMILY_NAME        DATABASE_NAME     ENVIRONMENT
+--    SAMPLE_DATA       SAMPLE_DATA_DEV       DEV
+--    SAMPLE_DATA       SAMPLE_DATA_UAT       UAT
+--    SAMPLE_DATA       SAMPLE_DATA_PROD      PROD
+
+
+-- What objects changed in SAMPLE_DATA_DEV between 2024-05-01 and 2024-05-10?
+CALL _CODE.SCHEMA_COMPARE(SAMPLE_DATA_DEV, '2024-05-01 00:00:00', SAMPLE_DATA_DEV, '2024-05-10 00:00:00', FALSE);
+
+
+-- Compare SAMPLE_DATA_DEV and SAMPLE_DATA_UAT on 2024-05-10?
+CALL _CODE.SCHEMA_COMPARE(SAMPLE_DATA_DEV, '2024-05-10 00:00:00', SAMPLE_DATA_UAT, '2024-05-10 00:00:00', FALSE);
+
+
+-- Record the Report ID that is returned from the procedure.  You will need it to get the details.
